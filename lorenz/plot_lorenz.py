43f910204326c2dfd4a614d7b3a0ca3ba185e3ff
“SebastianDall” <semoda@bio.auu.dk>
2023-06-29 08:57:00 +0000

    added 2d plot with timeresolved coloring

--- lorenz/plot_lorenz.py
+++ lorenz/plot_lorenz.py
@@ -102,8 +102,11 @@ def plot_lorenz2D(lorenz: Lorenz, directions: list = ["xy", "xz", "yz"]):
     # Unpack the history into separate x, y, and z lists
     x, y, z = zip(*lorenz.history)
 
-    # Create a color array (from 0 to 1) one for each point in the trajectory
-    colors = plt.cm.viridis(np.linspace(0, 1, len(x)))
+    # Create a time array for each point in the trajectory
+    time = np.arange(len(x)) * lorenz.dt
+
+    # Normalize time array for color mapping
+    time_norm = time / time.max()
 
     # Create 1 row by 3 columns subplots
     fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
@@ -111,15 +114,21 @@ def plot_lorenz2D(lorenz: Lorenz, directions: list = ["xy", "xz", "yz"]):
     for index, direction in enumerate(directions):
         ax = axs[index]
         if direction == "xy":
-            ax.plot(x, y, color="blue")
+            sc = ax.scatter(
+                x, y, c=time_norm, cmap="viridis", s=5
+            )  # use time for color map
             ax.set_xlabel("X Axis")
             ax.set_ylabel("Y Axis")
         elif direction == "xz":
-            ax.plot(x, z, color="green")
+            sc = ax.scatter(
+                x, z, c=time_norm, cmap="viridis", s=5
+            )  # use time for color map
             ax.set_xlabel("X Axis")
             ax.set_ylabel("Z Axis")
         elif direction == "yz":
-            ax.plot(y, z, color="red")
+            sc = ax.scatter(
+                y, z, c=time_norm, cmap="viridis", s=5
+            )  # use time for color map
             ax.set_xlabel("Y Axis")
             ax.set_ylabel("Z Axis")
         else:
@@ -127,12 +136,62 @@ def plot_lorenz2D(lorenz: Lorenz, directions: list = ["xy", "xz", "yz"]):
 
         ax.set_title(f"Lorenz Attractor ({direction})")
 
+    # Create a colorbar on the right side
+    cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.7])
+    sm = plt.cm.ScalarMappable(
+        cmap=plt.cm.viridis, norm=plt.Normalize(time.min(), time.max())
+    )
+    sm.set_array([])
+    fig.colorbar(sm, cax=cbar_ax, label="Time")
+
     plt.tight_layout()  # Ensure subplots do not overlap
+    plt.subplots_adjust(right=0.9)  # Adjust the right boundary of the plot window
     plt.show()
 
 
+# def animate_lorenz(lorenz: Lorenz):
+#     """Plot the trajectory of the Lorenz system."""
+
+#     # Ensure the system has been solved before plotting
+#     if not lorenz.history:
+#         lorenz.solve()
+
+#     # Unpack the history into separate x, y, and z lists
+#     x, y, z = zip(*lorenz.history)
+
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection="3d")
+
+#     # Create a color array (from 0 to 1) one for each point in the trajectory
+#     colors = plt.cm.viridis(np.linspace(0, 1, len(x)))
+
+#     (line,) = ax.plot([], [], [], color="blue")
+
+#     ax.set_xlim([min(x), max(x)])
+#     ax.set_ylim([min(y), max(y)])
+#     ax.set_zlim([min(z), max(z)])
+
+#     ax.set_xlabel("X Axis")
+#     ax.set_ylabel("Y Axis")
+#     ax.set_zlabel("Z Axis")
+#     ax.set_title("Lorenz Attractor")
+
+#     def animate(i):
+#         line.set_data(x[:i], y[:i])
+#         line.set_3d_properties(z[:i])
+#         line.set_color(colors[i])
+#         return (line,)
+
+#     anim = FuncAnimation(fig, animate, frames=len(x), interval=30, blit=True)
+
+#     # Save the animation
+#     anim.save("lorenz_attractor.gif", writer="imagemagick")
+
+#     plt.show()
+
+
 def animate_lorenz(lorenz: Lorenz):
-    """Plot the trajectory of the Lorenz system."""
+    """Animate the trajectory of the Lorenz system."""
 
     # Ensure the system has been solved before plotting
     if not lorenz.history:
@@ -147,7 +206,11 @@ def animate_lorenz(lorenz: Lorenz):
     # Create a color array (from 0 to 1) one for each point in the trajectory
     colors = plt.cm.viridis(np.linspace(0, 1, len(x)))
 
-    (line,) = ax.plot([], [], [], color="blue")
+    # Add a line object for the trajectory
+    (line,) = ax.plot([], [], [], color="blue", lw=2)
+
+    # Add a point object for the current position
+    (point,) = ax.plot([], [], [], color="red", marker="o")
 
     ax.set_xlim([min(x), max(x)])
     ax.set_ylim([min(y), max(y)])
@@ -159,10 +222,17 @@ def animate_lorenz(lorenz: Lorenz):
     ax.set_title("Lorenz Attractor")
 
     def animate(i):
+        # Update the line data
         line.set_data(x[:i], y[:i])
         line.set_3d_properties(z[:i])
         line.set_color(colors[i])
-        return (line,)
+
+        # Update the point data
+        point.set_data(x[i], y[i])
+        point.set_3d_properties(z[i])
+        point.set_color(colors[i])
+
+        return line, point
 
     anim = FuncAnimation(fig, animate, frames=len(x), interval=30, blit=True)
 
