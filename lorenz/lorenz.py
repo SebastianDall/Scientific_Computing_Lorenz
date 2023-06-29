5e64535981836b4d5c78137d6e0afb6988bfe9f8
“SebastianDall” <semoda@bio.auu.dk>
2023-06-29 07:13:08 +0000

    Added solver

--- lorenz/lorenz.py
+++ lorenz/lorenz.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 from numba import jit
 
 
@@ -34,6 +34,7 @@ class Lorenz:
         beta: float = 2.667,
         rho: float = 28.0,
         dt: float = 0.01,
+        N: int = 5000,
     ) -> None:
         """
         Constructs all the necessary attributes for the Lorenz object.
@@ -50,14 +51,48 @@ class Lorenz:
                 The rho parameter of the Lorenz system (default is 28.0).
             dt : float, optional
                 The time step for the simulation (default is 0.01).
+            N : int, optional
         """
         self.state = initial_state
         self.sigma = sigma
         self.beta = beta
         self.rho = rho
         self.dt = dt
+        self.N = N
 
-    @jit(nopython=True)
+        self.history = []
+
+    def solve(self, method: str = "euler") -> List[Tuple[float, float, float]]:
+        """
+        Solves the Lorenz system for N time steps.
+
+        Parameters
+        ----------
+        method : str, optional
+            The integration method to use (either "euler" or "rk4").
+            Default is "euler".
+
+        Returns
+        -------
+        list of tuple of float
+            The state of the system (x, y, z) at each time step.
+        """
+        if method not in ("euler", "rk4"):
+            raise ValueError('Method must be either "euler" or "rk4"')
+
+        self.history = []  # Reset the history at the beginning of each simulation
+        self.history.append(self.state)  # Record the initial state
+
+        for _ in range(self.N):
+            if method == "euler":
+                self.step_euler()
+            else:
+                self.step_rk4()
+            self.history.append(self.state)  # Record the state after each step
+
+        return self.history
+
+    # @jit(nopython=True)
     def step_euler(self) -> Tuple[float, float, float]:
         """
         Advances the state of the system one time step using the Euler method.
